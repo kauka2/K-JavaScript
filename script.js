acfbfc01a7f014874d02da7a83be75727820eb29
kauka2 <barbosak2526@gmail.com>
2023-06-02 10:24:14 -0300

    JAva

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
 const listElement = document.querySelector("#app ul")
 const inputElement = document.querySelector("#app input")
-const buttonElement =  document.querySelector("#app button")
+const buttonElement = document.querySelector("#app button")
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+
 
 //console.log(listElement, inputElement, buttonElement)
 
-const tarefas = ["Estudar pra prova", "Dormir cedo"]
+//const tarefas = ["Estudar pra prova", "Dormir cedo"]
 
-for (const itemTarefa of tarefas) {
-    //console.log(itemTarefa)
-    const tarefaElement = document.createElement("li")
-    const tarefaText = document.createTextNode(itemTarefa)
-    
-    tarefaElement.appendChild(tarefaText)
-    listElement.appendChild(tarefaElement)
+function renderTarefas() {
+    listElement.innerHTML = ""
+    for (const itemTarefa of tarefas) {
+        //console.log(itemTarefa)
+        const tarefaElement = document.createElement("li")
+        const tarefaText = document.createTextNode(itemTarefa)
+        const linkElement = document.createElement("a")
+        linkElement.setAttribute("href", "#")
+        const pos = tarefas.indexOf(itemTarefa)
+        linkElement.setAttribute("onclick", `deleteTarefa(${pos})`)
+        const linkText = document.createTextNode("-Apagar")
+        linkElement.appendChild(linkText)
+
+        tarefaElement.appendChild(tarefaText)
+        tarefaElement.appendChild(linkElement)
+        listElement.appendChild(tarefaElement)
+    }
+}
+renderTarefas()
+function addTarefas() {
+    const text = inputElement.value
+    tarefas.push(text)
+    renderTarefas()
+    inputElement.value = ""
+    saveToStorage()
+}
+buttonElement.onclick = addTarefas
+function deleteTarefa(pos) {
+    delete tarefas[pos]
+    tarefas.splice(pos, 1)
+    renderTarefas()
+    saveToStorage()   
+}
+function saveToStorage() {
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+
+
 }
